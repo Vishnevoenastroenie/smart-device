2e374332f14868ef63b39e500a3828a18b185c94
Наталья Лягина <Vischnevoenastroenie@yandex.ru>
2023-04-19 02:32:19 +0400

    Вносит исправления до полуправильного попапа

--- source/js/modules/form-validate/form.js
+++ source/js/modules/form-validate/form.js
@@ -92,13 +92,13 @@ export class Form {
     const phoneParents = form.querySelectorAll('[data-validate-type="phone"]');
     phoneParents.forEach((item) => this._initPhoneInput(item));
 
-    const callback = parent.dataset.callback;
-    form.noValidate = true;
+    // const callback = parent.dataset.callback;
+    // form.noValidate = true;
 
-    form.addEventListener('submit', (event) => {
-      event.preventDefault();
-      this._onFormSubmit(event, callback);
-    });
+    // form.addEventListener('submit', (event) => {
+    // event.preventDefault();
+    // this._onFormSubmit(event, callback);
+    // });
 
     form.addEventListener('input', (event) => {
       this._onFormInput(event.target);
